f0e49114d57b6b9b19a8c009b80181a26c02c104
Joseph Heck <heckj@mac.com>
2025-11-25 15:01:33 -0800

    adding baseline fixtures for testing parsing of files (#2)
    * adding baseline fixtures for testing parsing of files
    * adding in basic fixtures, verifying how #bundle macro works
    * bundle macro apparently not yet available
    * note for future me -quickcheck test locally for Linux

--- Tests/DocCArchiveTransformerTests/DocCArchiveTransformerTests.swift
+++ Tests/DocCArchiveTransformerTests/DocCArchiveTransformerTests.swift
@@ -1,7 +1,12 @@
 import Testing
+import VendoredDocC
 
-@testable import DocCArchiveTransformer
-
-@Test func example() async throws {
-  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func verifyVersionExists() async throws {
+    #expect(!VendoredDocC.VendoredVersion.description.isEmpty)
+    #expect(!VendoredDocC.VendoredVersion.commit.isEmpty)
 }
+
+//@Test func example() async throws {
+//    let testBundle = #bundle
+//    print(testBundle.bundlePath)
+//}
