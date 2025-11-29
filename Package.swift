2397a647f5a387d1f207225eae250ff112501002
Joe Heck <heckj@mac.com>
2025-11-29 11:42:48 -0800

    API noodling and package prep

--- Package.swift
+++ Package.swift
@@ -5,24 +5,28 @@ import PackageDescription
 let package = Package(
   name: "DocCArchive",
   platforms: [
-    .macOS(.v13)
+    .macOS(.v14)
   ],
   products: [
     .library(
       name: "DocCArchive",
       targets: ["DocCArchive"]
-    )
+    ),
+    .executable(name: "splat", targets: ["ProcessArchive"]),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.10.0"),
     .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.0"),
     .package(url: "https://github.com/apple/swift-nio", from: "2.0.0"),
+    .package(url: "https://github.com/sliemeobn/elementary.git", from: "0.3.2"),
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.0"),
   ],
   targets: [
     .target(
       name: "DocCArchive",
       dependencies: [
         "VendoredDocC",
+        .product(name: "Elementary", package: "elementary"),
         .product(name: "_NIOFileSystem", package: "swift-nio"),
       ]
     ),
@@ -49,5 +53,14 @@ let package = Package(
       dependencies: ["DocCArchive"],
       resources: [.copy("Fixtures")]
     ),
+    .executableTarget(
+      name: "ProcessArchive",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "_NIOFileSystem", package: "swift-nio"),
+        .product(name: "Elementary", package: "elementary"),
+        "VendoredDocC",
+        "DocCArchive",
+      ]),
   ]
 )
---
+++ Sources/ProcessArchive/Untitled.swift
@@ -0,0 +1,25 @@
+import ArgumentParser
+
+@main
+struct Repeat: ParsableCommand {
+  @Flag(help: "Include a counter with each repetition.")
+  var includeCounter = false
+
+  @Option(name: .shortAndLong, help: "How many times to repeat 'phrase'.")
+  var count: Int? = nil
+
+  @Argument(help: "The phrase to repeat.")
+  var phrase: String
+
+  mutating func run() throws {
+    let repeatCount = count ?? 2
+
+    for i in 1...repeatCount {
+      if includeCounter {
+        print("\(i): \(phrase)")
+      } else {
+        print(phrase)
+      }
+    }
+  }
+}
